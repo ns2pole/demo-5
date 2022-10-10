df30f3a2744ee3ce937c87e59960ffd0dc32bb66
中村俊介 <nakamura@feliseed.co.jp>
2022-10-10 15:50:58 +0900

    first commit

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,14 @@
+const element1 = document.getElementById('attendwork');
+element1.addEventListener('click', function() {
+    window.confirm('出勤しますか？');
+})
+
+const element2 = document.getElementById('leavingwork');
+element2.addEventListener('click', function() {
+    window.confirm('退勤しますか？');
+})
+document.getElementById("nowTime").innerHTML = new Date().toLocaleString();
+function showDate() {
+    document.getElementById("nowTime").innerHTML = (new Date()).toLocaleString();
+    window.setTimeout("showDate()" , 1000);
+}    
