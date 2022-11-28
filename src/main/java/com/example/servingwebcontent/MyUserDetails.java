a0157b18936dbebe75ce56380a060a2b55a39696
中村俊介 <syunsukerunrun0306@icloud.com>
2022-11-28 20:53:30 +0900

    [権限制御]userとadmin権限を構築。adminのデータをuserと同様作成したら使える。

--- src/main/java/com/example/servingwebcontent/MyUserDetails.java
+++ src/main/java/com/example/servingwebcontent/MyUserDetails.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.authority.AuthorityUtils;
 
 public class MyUserDetails implements UserDetails {
 
@@ -15,10 +16,6 @@ public class MyUserDetails implements UserDetails {
 		this.user = user;
 	}
 
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return null;
-	}
 
 	@Override
 	public String getPassword() {
@@ -48,6 +45,11 @@ public class MyUserDetails implements UserDetails {
 	@Override
 	public boolean isEnabled() {
 		return true;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return AuthorityUtils.createAuthorityList(user.getAuth());
 	}
 
 }
