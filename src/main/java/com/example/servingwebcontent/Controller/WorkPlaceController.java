ac4fce97f5d7365db406330afc5aaca9a4ce6153
中村俊介 <syunsukerunrun0306@icloud.com>
2022-12-05 23:56:22 +0900

    [雛形の生成]user,部署,勤務場所管理[リファクタ]workplaceをキャメルケースへ

--- src/main/java/com/example/servingwebcontent/Controller/WorkPlaceController.java
+++ src/main/java/com/example/servingwebcontent/Controller/WorkPlaceController.java
@@ -2,14 +2,12 @@ package com.example.servingwebcontent.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.ui.Model;
 
 @Controller
-public class LoginController {
-
-	@GetMapping("/loginForm")
-	public String loginForm(Model model) {
-		return "loginForm";
+public class WorkPlaceController {
+	@GetMapping("/workPlace")
+	public String workPlace() {
+		return "workPlace";
 	}
 
 }
