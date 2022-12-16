c289ff753c685fd9a88b9dde6c85ada53e23bf73
中村俊介 <nakamura@feliseed.co.jp>
2022-12-16 11:52:17 +0900

    workplaceをenumで管理することにした。

---
+++ src/main/java/com/example/servingwebcontent/enums/WorkPlace.java
@@ -0,0 +1,2 @@
+package com.example.servingwebcontent.enums;public enum Workplace {
+}
--- src/main/java/com/example/servingwebcontent/repository/WorkPlaceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.servingwebcontent.repository;
-
-import com.example.servingwebcontent.model.WorkPlace;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WorkPlaceRepository extends JpaRepository<WorkPlace, Integer> {
-}
